cb72c96543b3fc1a5bd781850b5fc7236494d7db
Christopher Jackson <cjacksoncmyk@gmail.com>
2023-08-15 20:52:08 -0400

    user input

---
+++ helpers.py
@@ -0,0 +1,11 @@
+username_helper = """
+MDTextField:
+    hint_text: "Enter profile name"
+    helper_text: "or select existing profile"
+    helper_text_mode:"on_focus"
+    icon_right: "chess-king"
+    icon_right_color: app.theme_cls.primary_color
+    pos_hint:{'center_x': 0.5, 'center_y': 0.5}
+    size_hint_x:None
+    width:300
+"""
